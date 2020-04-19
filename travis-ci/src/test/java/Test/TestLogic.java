df5d4cf35f30e72d3a6d86a575370e4ab45e5e1f
mariamshabrawe <mariamshabrawe11@hotmail.com>
2020-04-19 14:58:03 +0200

    first commit

---
+++ travis-ci/src/test/java/Test/TestLogic.java
@@ -0,0 +1,18 @@
+package Test;
+
+
+ 
+
+import static org.junit.Assert.*;  
+import Calc.Calculation;
+
+import org.junit.Test;  
+  
+public class TestLogic {  
+  
+    @Test  
+    public void testFindMax(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+}
